81b022ecf0fd8096e3192192d95ce3a85640e7ff
goeln <natasha5@illinois.edu>
2021-07-28 22:29:37 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 const margin = {top: 20, right: 120, bottom: 50, left: 50},
-    svgWidth = 900,
-    svgHeight = 600,
+    svgWidth = 800,
+    svgHeight = 500,
     width = svgWidth - margin.left - margin.right,
     height = svgHeight - margin.top - margin.bottom;
 
