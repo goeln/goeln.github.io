bd9232066231b479fd21c0aa6a87b7489d90778d
goeln <natasha5@illinois.edu>
2021-07-30 18:56:00 -0500

    Update code.js

--- code.js
+++ code.js
@@ -249,7 +249,7 @@ function drawChartAnnotationWorld(countryCode, countrylabel, color){
             .attr("transform",
                 "translate(" + (width/2) + " ," + 
                                 ((height/2) - 48) + ")")
-            .style('fill', 'purple')
+            .style('fill', 'indigo')
             .text("There's 8% increase from 2000 to 2018");
         
         innerChart
@@ -257,7 +257,7 @@ function drawChartAnnotationWorld(countryCode, countrylabel, color){
             .attr("transform",
                 "translate(" + (width/2) + " ," + 
                                 ((height/2) - 28) + ")")
-            .style('fill', 'purple')
+            .style('fill', 'indigo')
             .text("but only a ~1% difference between the genders");
         
         innerChart
@@ -342,7 +342,7 @@ function drawChartAnnotationUSA(countryCode, countrylabel, color){
             .attr("transform",
                 "translate(" + (width/2) + " ," + 
                                 ((height/2) - 145) + ")")
-            .style('fill', 'purple')
+            .style('fill', 'indigo')
             .text("The % difference in employment from 2000 to");
         
         innerChart
@@ -350,7 +350,7 @@ function drawChartAnnotationUSA(countryCode, countrylabel, color){
             .attr("transform",
                 "translate(" + (width/2) + " ," + 
                                 ((height/2) - 125) + ")")
-            .style('fill', 'purple')
+            .style('fill', 'indigo')
             .text(" 2018 is only about 1-2% across both genders");
 
         innerChart
